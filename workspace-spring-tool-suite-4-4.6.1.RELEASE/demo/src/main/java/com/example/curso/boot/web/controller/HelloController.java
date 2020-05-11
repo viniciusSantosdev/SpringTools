5ec813027ffad676b39d53bf85c965c144d08742
Vinicius Santos <santos.viniiusmt@gmail.com>
2020-05-11 10:58:43 -0300

    Create the base of the project

---
+++ workspace-spring-tool-suite-4-4.6.1.RELEASE/demo/src/main/java/com/example/curso/boot/web/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.curso.boot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/")
+	public String home(){
+		
+		return "/home";
+		
+	}
+
+}
